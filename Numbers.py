0b49ec83ebe6d19fb44de94a2e6d997f7a35580d
Palpatine <alvingitonga14@gmail.com>
2022-04-05 09:15:36 +0300

    Update Numbers.py

--- Numbers.py
+++ Numbers.py
@@ -1,1 +1,10 @@
-<>
+# There are three numeric types in Python; Variables of numeric types are created when you assign a value to them
+
+x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
+
+# To verify the type of any object in Python, use the type() function
+print(type(x))
+print(type(y))
+print(type(z))
