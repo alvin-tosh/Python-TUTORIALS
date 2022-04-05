c93385e631dc7a6e25a4a058ffe2cdd2edccf562
Palpatine <alvingitonga14@gmail.com>
2022-04-05 08:48:32 +0300

    Update Input.py
    Example of inut() expression in the interactive shell

--- Input.py
+++ Input.py
@@ -1,1 +1,10 @@
-<>
+#Examples of expressions in the interactive shell:
+
+
+>>> print('What is your name?')   # ask for their name
+>>> myName = input()
+>>> print('It is good to meet you, {}'.format(myName))
+
+# What is your name?
+#Al
+#It is good to meet you, Al
