84cc814ec7a85c70ab5aa672b241729852c58d1b
Palpatine <alvingitonga14@gmail.com>
2022-04-05 09:00:30 +0300

    Update Variables.py

--- Variables.py
+++ Variables.py
@@ -1,1 +1,11 @@
-<>
+# Python has no command for declaring a variable.
+# A variable is created the moment you first assign a value to it.
+# Variables do not need to be declared with any particular type, and can even change type after they have been set.
+
+x = 4           # x is of type int
+x = "Sally"     # x is now of type str
+print(x)        # Output: Sally
+
+
+
+
