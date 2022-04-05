11c8d6b4985bbf9e5e557b0f1eac299bd6aaae34
Palpatine <alvingitonga14@gmail.com>
2022-04-05 09:24:46 +0300

    Update Numbers.py

--- Numbers.py
+++ Numbers.py
@@ -9,3 +9,35 @@ z = 1j    # complex numbers are written with a "j" as the imaginary part
 print(type(x))
 print(type(y))
 print(type(z))
+
+
+
+# Type Conversion: You can convert from one type to another with the int(), float(), and complex() methods.
+# NOTE: You cannot convert complex numbers into another number type!
+x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
+
+#convert from int to float:
+a = float(x)
+
+#convert from float to int:
+b = int(y)
+
+#convert from int to complex:
+c = complex(x)
+
+print(a)
+print(b)
+print(c)
+
+print(type(a))
+print(type(b))
+print(type(c)) 
+
+
+
+# Python has a built-in module called random that can be used to make random numbers.
+import random
+
+print(random.randrange(1, 10)) # ImportS the random module, and displayS a random number between 1 and 9
