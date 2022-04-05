980343736c69c721c5b2aa486eaee01938cdbbac
Palpatine <alvingitonga14@gmail.com>
2022-04-05 09:32:20 +0300

    Update Bool.py

--- Bool.py
+++ Bool.py
@@ -1,1 +1,17 @@
-<>
+# In programming you often need to know if an expression is True or False. 
+# You can evaluate any expression in Python, and get one of two answers, True or False.
+# When you compare two values, the expression is evaluated and Python returns the Boolean answer
+
+print(10 > 9)  # True
+print(10 == 9) # False 
+print(10 < 9)  # False
+
+
+
+# The bool() function allows you to evaluate any value, and give you True or False in return.
+x = "Hello"
+y = 15
+
+print(bool(x)) # True
+print(bool(y)) # True
+
