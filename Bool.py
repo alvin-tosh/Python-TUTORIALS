e8ab980d748d5c3a1cf1814506ae55773873c3f4
Palpatine <alvingitonga14@gmail.com>
2022-04-05 09:35:05 +0300

    Update Bool.py

--- Bool.py
+++ Bool.py
@@ -15,3 +15,21 @@ y = 15
 print(bool(x)) # True
 print(bool(y)) # True
 
+
+
+# Almost any value is evaluated to True if it has some sort of content.
+.......................................................................
+# Any string is True, except empty strings.
+......................................................................
+# Any number is True, except 0.
+......................................................................
+# Any list, tuple, set, and dictionary are True, except empty ones.
+......................................................................
+bool("abc")
+bool(123)
+bool(["apple", "cherry", "banana"])
+# All will return True
+
+
+
+#FYI: There are not many values that evaluate to False, except empty values, such as (), [], {}, "", the number 0, and the value None. And of course the value False evaluates to False.
