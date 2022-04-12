0f34f671de9a88df97bf1f86d653164ff29beef7
Palpatine <alvingitonga14@gmail.com>
2022-04-12 09:29:02 +0300

    Update Lists.py
    TO BE CONTINUED

--- Lists.py
+++ Lists.py
@@ -1,1 +1,14 @@
-<>
+# A list is an ordered collection of items.
+# Python uses the square brackets ([]) to indicate a list. The following shows an empty list:
+
+empty_list = []
+
+# Typically, a list contains one or more items. 
+# To separate two items, you use a comma (,). For example:
+
+todo_list = ['Learn Python List','How to manage List elements']
+
+# Since a list often contains many items, it’s a good practice to name it using plural nouns e.g., numbers, colors, and shopping_carts.
+# The following example defines a list of six numbers:
+
+numbers = [1, 3, 2, 7, 9, 4]
