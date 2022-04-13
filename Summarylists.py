64545e249db529a53f185e357ad352d69083c784
Palpatine <alvingitonga14@gmail.com>
2022-04-13 09:51:04 +0300

    Update Summarylists.py
    Python Lists Cheat-sheet
    
    Be awesome!

--- Summarylists.py
+++ Summarylists.py
@@ -1,1 +1,22 @@
-<>
+# Summary or rather --- Python lists Cheat sheet ---
+
+    # A list is an ordered collection of items.
+  
+    # Use square bracket notation [] to access a list element by its index. The first element has an index 0.
+    
+    # Use a negative index to access a list element from the end of a list. The last element has an index -1.
+    
+    # Use list[index] = new_value to modify an element from a list.
+    
+    # Use append() to add a new element to the end of a list.
+    
+    # Use insert() to add a new element at a position in a list.
+    
+    # Use pop() to remove an element from a list and return that element.
+    
+    # Use remove() to remove an element from a list.
+    
+
+Next is Tuples....you ready? ---i will see you at Tuples.py
+
+BREAK A LEG :)
