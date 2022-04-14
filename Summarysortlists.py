b481b2ee6cd2dec49070499459016372d09dca36
Palpatine <alvingitonga14@gmail.com>
2022-04-14 10:09:43 +0300

    Update Summarysortlists.py
    wrapping up them sort() algrithms with key points...sort of....;) see what i did there...hihi...see you in Slicelists.py
    
    have an awesome one!

--- Summarysortlists.py
+++ Summarysortlists.py
@@ -1,1 +1,13 @@
-<>
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# Summary
+
+    # Use the Python List sort() method to sort a list in place.
+    # The sort() method sorts the string elements in alphabetical order and sorts the numeric elements from smallest to largest.
+    # Use the sort(reverse=True) to reverse the default sort order.
+    # A function without a name is called an anonymous function. And this syntax is called a lambda expression.
+    # Suppose that you have a list of tuples-------------; 
+      # First, specify a sort key and pass it to the sort() method. To define a sort key, you create a function that accepts a tuple and returns the element that you want to sort by.
+      # Second, pass the sort_key function to the sort() method
+      # The sort() method will use the value returned by the sort_key() function for the comparisons.
+      
+    # REMEMBER that you just pass the function name without the parentheses to the sort() method.
