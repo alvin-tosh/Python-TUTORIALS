57a993967bfe335810ac2b619058845f0b3ff9a5
Palpatine <alvingitonga14@gmail.com>
2022-04-14 09:00:38 +0300

    Update Tuples.py

--- Tuples.py
+++ Tuples.py
@@ -1,1 +1,4 @@
-<>
+# A tuple is a list that cannot change. Python refers to a value that cannot change as immutable. So by definition, a tuple is an immutable list.
+# A tuple is like a list except that it uses parentheses () instead of square brackets []...ie
+
+
