46aa820ad4d74f870bf91817ed86c9fca9c775d3
Palpatine <alvingitonga14@gmail.com>
2022-04-14 09:30:50 +0300

    Update Summarytuples.py
    Summary of tuples and how they are leopards.....coz their spots dont change ;)

--- Summarytuples.py
+++ Summarytuples.py
@@ -1,1 +1,17 @@
-<>
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# Summary
+
+    # Tuples are immutable lists.
+    # A tuple is like a list except that it uses parentheses () instead of square brackets [].
+    rgb = ('red', 'green', 'blue')
+    # Use tuples when you want to define a list that cannot change.
+    # Even though you can’t change a tuple, you can assign a new tuple to a variable that references a tuple.
+    # To define a tuple with one element, you need to include a trailing comma after the first element. 
+    numbers = (3,) # Tuple with single element
+    # If you exclude the trailing comma, the type of the numbers will be int , which stands for integer. And its value is 3. Python won’t create a tuple that includes the number 3
+    numbers = (3) # Python won’t create a tuple that includes the number 3
+    
+    
+# Good now you got tuples...lets ride this high! Next is Sorting a list...and i will see you at Sortinglists.py
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    
